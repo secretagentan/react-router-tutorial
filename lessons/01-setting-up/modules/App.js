33b8e82b0c94487f31ca0fc5c3d303d68efc23b9
an nguyen <hello.an.ng@gmail.com>
2017-02-28 11:23:28 -0800

    Revert "saves tutorial progress up to production-ish server"
    This reverts commit 267f39c11118f52c5f88bfae1bdce33962536291.

--- lessons/01-setting-up/modules/App.js
+++ lessons/01-setting-up/modules/App.js
@@ -1,31 +1,7 @@
-import React from 'react';
-import { Link } from 'react-router';
-import NavLink from './NavLink';
-import Home from './Home';
-import { IndexLink } from 'react-router';
+import React from 'react'
 
-// export default React.createClass({
-//   render() {
-//     return <div>Hello World</div>
-//   }
-// })
-
-class App extends React.Component{
+export default React.createClass({
   render() {
-    return(
-      <div>
-        <h1>React Router Tutorial</h1>
-        <ul role="nav">
-          <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
-          <li><NavLink to="/" onlyActiveOnIndex={true}>Home</NavLink></li>
-          <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
-          <li><NavLink to="/repos" activeStyle={{ color: 'red' }}>Repos</NavLink></li>
-        </ul>
-        {this.props.children}
-        {this.props.children || <Home />}
-      </div>
-    );
+    return <div>Hello, React Router!</div>
   }
-}
-
-export default App;
+})
