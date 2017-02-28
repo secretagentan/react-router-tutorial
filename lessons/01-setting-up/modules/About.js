10734705ba7247a230328bf7ae3acf955cf98e44
an nguyen <hello.an.ng@gmail.com>
2017-02-28 11:25:43 -0800

    what happened

---
+++ lessons/01-setting-up/modules/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// export default React.createClass({
+//   render() {
+//     return <div>About</div>
+//   }
+// });
+
+class About extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+      <div>About</div>
+    );
+  }
+}
+
+export default About;
