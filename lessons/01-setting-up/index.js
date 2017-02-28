267f39c11118f52c5f88bfae1bdce33962536291
an nguyen <hello.an.ng@gmail.com>
2017-02-28 10:49:04 -0800

    saves tutorial progress up to production-ish server

--- lessons/01-setting-up/index.js
+++ lessons/01-setting-up/index.js
@@ -1,4 +1,21 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './modules/App'
-render(<App/>, document.getElementById('app'))
+import React from 'react';
+import { render } from 'react-dom';
+import App from './modules/App';
+import About from './modules/About';
+import Repos from './modules/Repos';
+import Home from './modules/Home';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+
+render(<App/>, document.getElementById('app'));
+
+render((
+  <Router history={browserHistory}>
+    <Route path='/' component={App}>
+      <IndexRoute component={Home} />
+      <Route path='/about' component={About} />
+      <Route path='/repos' component={Repos}>
+        <Route path='/repos/:userName/:repoName' component={Repos} />
+      </Route>
+    </Route>
+  </Router>
+), document.getElementById('app'));
--- lessons/01-setting-up/modules/App.js
+++ lessons/01-setting-up/modules/App.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router';
+import NavLink from './NavLink';
+import Home from './Home';
+import { IndexLink } from 'react-router';
 
-export default React.createClass({
+// export default React.createClass({
+//   render() {
+//     return <div>Hello World</div>
+//   }
+// })
+
+class App extends React.Component{
   render() {
-    return <div>Hello, React Router!</div>
+    return(
+      <div>
+        <h1>React Router Tutorial</h1>
+        <ul role="nav">
+          <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
+          <li><NavLink to="/" onlyActiveOnIndex={true}>Home</NavLink></li>
+          <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
+          <li><NavLink to="/repos" activeStyle={{ color: 'red' }}>Repos</NavLink></li>
+        </ul>
+        {this.props.children}
+        {this.props.children || <Home />}
+      </div>
+    );
   }
-})
+}
+
+export default App;
